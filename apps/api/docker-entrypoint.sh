065666bae0555b228b830e971f762c7e0a7c210d
AstosWeb <phartemo@gmail.com>
2026-07-29 23:15:42 +0300

    changse

--- apps/api/docker-entrypoint.sh
+++ apps/api/docker-entrypoint.sh
@@ -6,7 +6,7 @@ chown -R hero:hero /app/.data
 
 if [ "${SKIP_DB_PUSH}" != "true" ]; then
   echo "Applying database schema..."
-  runuser -u hero -- ./node_modules/.bin/prisma db push --skip-generate
+  runuser -u hero -- ./node_modules/.bin/prisma db push
 fi
 
 exec runuser -u hero -- "$@"
